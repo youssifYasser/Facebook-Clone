19e9af6641641a1287898a83f5b2cccc4e48daac
youssef yasser <youssifyasserme1999@gmail.com>
2023-02-26 22:52:18 +0200

    truncate long post caption

--- components/Post.jsx
+++ components/Post.jsx
@@ -4,8 +4,10 @@ import {
   ShareIcon,
 } from '@heroicons/react/24/outline'
 import Image from 'next/image'
+import { useState } from 'react'
 
 const Post = ({ name, email, message, profileImage, postImage, timestamp }) => {
+  const [showMessage, setShowMessage] = useState(false)
   return (
     <div className='flex flex-col bg-white rounded-2xl shadow-md mt-6'>
       <div className='flex items-center space-x-2 p-3 sm:p-5'>
@@ -29,8 +31,21 @@ const Post = ({ name, email, message, profileImage, postImage, timestamp }) => {
         </div>
       </div>
 
-      <div className='pl-3 mb-2'>
-        <p className='whitespace-pre-line'>{message}</p>
+      <div className='px-3 mb-2'>
+        <p className='whitespace-pre-line'>
+          {showMessage ? message : message.slice(0, 50)}
+          {showMessage || (message.slice(50).length > 0 && '...')}
+          {message.slice(50).length > 0 && (
+            <span
+              className={`${
+                showMessage && 'hidden'
+              } underline cursor-pointer text-gray-600 ml-1`}
+              onClick={() => setShowMessage(true)}
+            >
+              more
+            </span>
+          )}
+        </p>
       </div>
 
       {postImage && (
